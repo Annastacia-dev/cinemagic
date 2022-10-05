5df9412b092fb87b0daa295e8a30acd2e1477595
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-05 10:34:37 +0300

    added new categories to movielist

--- src/components/Movies/MovieCard.js
+++ src/components/Movies/MovieCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MovieCard = ({poster_path, title}) => {
+const MovieCard = ({poster_path, title, vote_average}) => {
 
     const posterUrl = (posterpath) => {
         return(`https://www.themoviedb.org/t/p/w220_and_h330_face/${posterpath}`)
@@ -11,7 +11,7 @@ const MovieCard = ({poster_path, title}) => {
     <li>
     <div className='movie-card'>
         <img src={posterUrl(poster_path)} alt={title} />
-    </div>
+      </div>
     </li>
   )
 }
--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -4,23 +4,45 @@ import MovieCard from './MovieCard'
 
 const MovieList = () => {
 
-    const [movies, setMovies] = useState([])
+    const [popularMovies, setPopularMovies] = useState([])
 
+    // Popular
     useEffect(() => {
         axios.get('https://api.themoviedb.org/3/movie/popular?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US&page=1')
-        .then(resp => setMovies(resp.data.results))
+        .then(resp => setPopularMovies(resp.data.results.slice(0,6)))
         .catch(error => console.log(error))
         
     }, [])
 
-    console.log(movies)
+    // Top Rated
 
+    const [topRatedMovies, setTopRatedMovies] = useState([])
+
+    useEffect(() => {
+      axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US&page=1')
+      .then(resp => setTopRatedMovies(resp.data.results.slice(0,6)))
+      .catch(error => console.log(error))
+      
+  }, [])
+
+  // Upcoming Movies
+
+  const [upcomingMovies, setUpcomingMovies] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US&page=1')
+    .then(resp => setUpcomingMovies(resp.data.results.slice(-6)))
+    .catch(error => console.log(error))
+    
+}, [])
 
   return (
+    <>
+    {/* popular */}
     <div>
       <h3 className='category-title'>popular on cinemagic</h3>
         <div className='movie-container'>
-            {movies.map(movie => (
+            {popularMovies.map(movie => (
 
                 <>
                 <MovieCard key={movie.id} {...movie} />
@@ -29,6 +51,35 @@ const MovieList = () => {
             ))}
             </div>
     </div>
+
+    {/* top rated */}
+    <div>
+      <h3 className='category-title'>top rated on cinemagic</h3>
+        <div className='movie-container'>
+            {topRatedMovies.map(movie => (
+
+                <>
+                <MovieCard key={movie.id} {...movie} />
+                </>
+                
+            ))}
+            </div>
+    </div>
+
+    {/* upcoming */}
+    <div>
+      <h3 className='category-title'>upcoming on cinemagic</h3>
+        <div className='movie-container'>
+            {upcomingMovies.map(movie => (
+
+                <>
+                <MovieCard key={movie.id} {...movie} />
+                </>
+                
+            ))}
+            </div>
+    </div>
+    </>
   )
 }
 
