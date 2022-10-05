7ac6b493ce4d62bb6f8cae021d096b438feffc8e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-05 20:33:09 +0300

    movie item routing & styling

--- src/components/Movies/MovieItem.js
+++ src/components/Movies/MovieItem.js
@@ -4,9 +4,9 @@ import { useParams } from 'react-router-dom'
 const MovieItem = ({ movieData }) => {
 
     const params = useParams()
-  
-
+    
     return (
+      
         <div>
        
        {movieData.map(movie =>
@@ -14,16 +14,32 @@ const MovieItem = ({ movieData }) => {
        movie.id === parseInt(params.movieId) && (
         
         <>
-        <div className='movie-item'>
+        <div key={movie.id} className='movie-item'
+        style={{
+          'backgroundImage': `url(https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.backdrop_path}`,
+          'backgroundPosition': 'topRight',
+          'backgroundSize': '100%',
+          'backgroundRepeat': 'no-repeat',
+          }}>
+        
             <div className='movie-item-poster'>
                 <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt={movie.title} />
                 </div>
             <div className='movie-item-info'>
                 <h2>{movie.title}</h2>
-                <p>{movie.overview}</p>
+                <div className='movie-description'>
+                <p id='overview'>{movie.overview}</p>
                 <p>Release Date: {movie.release_date}</p>
                 <p>Rating: {movie.vote_average}</p>
+                <span>
+                  <button>Like</button>
+                </span>
+                <span>
+                  <p>0 likes</p>
+                </span>
+                </div>
                 
+
               </div>
           </div>
         </>
--- src/components/Movies/MoviesNavBar.js
+++ src/components/Movies/MoviesNavBar.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 
+
 const MoviesNavBar = ({ setMovieType}) => {
+
+
   return (
     <div className='movies-navbar'>
       <nav>
         <ul>
             <li>
-                <NavLink onClick={() => setMovieType("popular")} >Popular</NavLink>
-                <NavLink onClick={() => setMovieType("top_rated")} >Top rated</NavLink>
-                <NavLink onClick={() => setMovieType("upcoming")} >Upcoming</NavLink>
+                <NavLink onClick={() => setMovieType("popular")} >popular</NavLink>
+                <NavLink onClick={() => setMovieType("top_rated")} >top rated</NavLink>
+                <NavLink onClick={() => setMovieType("upcoming")} >latest</NavLink>
             </li>
         </ul>
       </nav>
