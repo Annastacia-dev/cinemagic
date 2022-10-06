e1c1485bda06146194b8df6086bc4aaf916fccef
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-06 16:15:25 +0300

    landing page

---
+++ src/cards.js
@@ -0,0 +1,31 @@
+import amsterdam from './assets/amsterdam.png'
+import avatar from './assets/avatar.png'
+import blackpanther from './assets/blackpanther.png'
+import horror from './assets/horror.png'
+import blackadam from './assets/blackadam.png'
+
+
+
+
+export const cards = [
+    {
+      id: "1",
+      image: amsterdam,
+    },
+    {
+      id: "2",
+      image: avatar,
+    },
+    {
+      id: "3",
+      image: blackpanther,
+    },
+    {
+        id: "4",
+        image: horror,
+      },
+      {
+        id: "5",
+        image: blackadam,
+      },
+  ];
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import '../css/landingpage.css';
+import { useEffect, useState } from "react";
+import { cards  } from '../cards';
 
-const LandingPage = () => {
+
+
+const LandingPage= () => {
+  const [index, setIndex] = useState(0);
+
+  const mod = (n, m) => {
+    let result = n % m;
+
+    // Return a positive value
+    return result >= 0 ? result : result + m;
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex((index + 1) % cards.length);
+      console.log(index);
+    }, 3000);
+  }, [index]);
+
   return (
-    <div>LandingPage</div>
-  )
+    <div className="App">
+      <div className="carousel">
+        {cards.map((item, i) => {
+          const indexLeft = mod(index - 1, cards.length);
+          const indexRight = mod(index + 1, cards.length);
+
+          let className = "card";
+
+          if (i === index) {
+            className = "card card--active";
+          } else if (i === indexRight) {
+            className = "card card--right";
+          } else if (i === indexLeft) {
+            className = "card card--left";
+          } else className = "card";
+
+          return (
+            <img
+              key={item.id}
+              className={className}
+              src={item.image}
+              alt="Comic"
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
+
